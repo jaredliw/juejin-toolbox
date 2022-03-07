a50804ea88cebac5139d0a130e36e31114c48e4b
jaredliw <jaredliw@gmail.com>
2022-03-07 11:55:03 +0800

    update (refer to gist)

--- script_old.py
+++ script_old.py
@@ -1,18 +1,14 @@
 from base64 import b64decode
-from itertools import chain, zip_longest
-from random import shuffle
+from copy import copy
+from itertools import chain
 from re import search
 from time import time, sleep
-from typing import List, Literal, Tuple, Union
+from typing import List, Literal, Tuple, Union, Generator
 
-from requests import post, RequestException
+from requests import post
 
 VALID_OPERATIONS = ("+", "-", "*", "/", "&")
 URL = "https://juejin-game.bytedance.com/game/num-puzz/ugc/start"
-
-
-class JuejinError(RequestException):
-    pass
 
 
 def calc(num1: int, sym: Literal["+", "-", "*", "/", "&"], num2: int) -> int:
@@ -36,7 +32,8 @@ def calc(num1: int, sym: Literal["+", "-", "*", "/", "&"], num2: int) -> int:
         return int(str(num1) + str(num2))
 
 
-def brute_force(target: int, nums: List[int], syms: List[Literal["+", "-", "*", "/", "&"]], timeout: int = 60) -> list:
+def brute_force(target: int, nums: List[int], syms: List[Literal["+", "-", "*", "/", "&"]]) \
+        -> Generator[List[Tuple[int, Literal["+", "-", "*", "/", "&"], int]], None, None]:
     if not all(map(lambda x: isinstance(x, int), nums)):
         raise ValueError("'nums' should only be comprised of integers")
     if not all(map(lambda x: x in VALID_OPERATIONS, syms)):
@@ -44,59 +41,58 @@ def brute_force(target: int, nums: List[int], syms: List[Literal["+", "-", "*", 
     if not isinstance(target, int):
         raise TypeError(f"'target' should be '{int.__name__}', not {type(target).__name__}")
 
-    try:
-        start_time = time()
-        for _ in range(len(nums) - len(syms) - 1):
-            syms.append("&")
+    def _inner(_nums, _syms, history=None):
+        if history is None:
+            history = []
+        if len(_nums) == 1 and len(_syms) == 0 and _nums[0] == target:
+            yield history
+            return
 
-        while True:
-            if time() - start_time > timeout:
-                raise TimeoutError("time limit exceeds")
-            shuffle(nums)
-            shuffle(syms)
-            exp = list(chain(*zip_longest(nums, syms)))[:-1]
-
-            precedence = list(range(1, len(syms) + 1))
-            shuffle(precedence)
-
-            steps = []
-            flag = False
-            for n in range(1, len(syms) + 1):
-                loc = precedence.index(n)
-                precedence.pop(loc)
-
-                loc *= 2
-                try:
-                    operands = [exp.pop(loc), exp.pop(loc), exp.pop(loc)]
-                    exp.insert(loc, calc(*operands))
-                    steps.append(" ".join(map(str, operands)))
-                except ArithmeticError:
-                    flag = True
-                    break
-            if flag:
+        _syms.extend(["&"] * (len(_nums) - len(_syms) - 1))
+        for sym in VALID_OPERATIONS:
+            if sym not in _syms:
                 continue
 
-            if exp[0] == target:
-                return steps
-    except (TimeoutError, KeyboardInterrupt) as e:
-        raise e
-    except:
-        raise ValueError("puzzle not solvable") from None
+            _syms_copied = copy(_syms)
+            _syms_copied.remove(sym)
+            for idx, num1 in enumerate(_nums[:-1]):
+                for num2 in _nums[idx + 1:]:
+                    for _ in range(2):
+                        try:
+                            result = calc(num1, sym, num2)
+                        except ArithmeticError:
+                            pass
+                        else:
+                            numbers_copied = copy(_nums)
+                            numbers_copied.remove(num1)
+                            numbers_copied.remove(num2)
+                            numbers_copied.append(result)
+
+                            history_copied = copy(history)
+                            history_copied.append(f"{num1} {sym} {num2}")
+
+                            yield from _inner(numbers_copied, _syms_copied, history_copied)
+
+                        if sym in ("-", "/", "&") and num1 != num2:
+                            num1, num2 = num2, num1
+                        else:
+                            break
+
+    return _inner(nums, syms)
 
 
 def fetch_data(authorization: str) -> dict:
     headers = {
         "authorization": authorization
     }
+    search_result = search(r'(?<="userId":")\d*', str(b64decode(authorization.removeprefix("Bearer ") + "==")))
+    if search_result is None:
+        raise ValueError("invalid token")
     params = {
-        "uid": search(r'(?<="userId":")\d*', str(b64decode(authorization.removeprefix("Bearer ") + "==")))[0],
+        "uid": search_result[0],
         "time": time() * 1000
     }
-    response = post(URL, headers=headers, params=params).json()
-    try:
-        return response["data"]
-    except KeyError:
-        raise JuejinError(response["message"]) from None
+    return post(URL, headers=headers, params=params).json()["data"]
 
 
 def resolve_map(game_map: List[List[Union[int, float]]]) -> Tuple[List[int], List[Literal["+", "-", "*", "/"]]]:
@@ -127,7 +123,8 @@ if __name__ == "__main__":
         if last_level != level:
             print("Level", level)
             print()
-            for step in brute_force(data["target"], *resolve_map(data["map"])):
+            steps = next(brute_force(data["target"], *resolve_map(data["map"])))
+            for step in steps:
                 print(step)
             print()
             last_level = level
